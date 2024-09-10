34fcc0aa6f1b462be78823e05f2944e853867ff6
Samaelish <semnd007@gmail.com>
2024-09-10 18:38:29 +0300

    moved everything inside todolist component

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,18 +1,69 @@
+import { useState, useEffect } from 'react'
 import TodoCard from './TodoCard'
+import TodoInput from './TodoInput'
 
-const TodoList = props => {
-  const { todos } = props
+const TodoList = () => {
+  const [todos, setTodos] = useState([])
+  const [todoValue, setTodoValue] = useState('')
+
+  useEffect(() => {
+    if (!localStorage) {
+      return
+    }
+
+    let localTodos = localStorage.getItem('todos')
+    if (!localTodos) {
+      return
+    }
+
+    console.log(localTodos)
+    localTodos = JSON.parse(localTodos).todos
+    setTodos(localTodos)
+  }, [])
+
+  function handleAddTodos(newTodo) {
+    const newTodoList = [...todos, newTodo]
+    persistData(newTodoList)
+    setTodos(newTodoList)
+  }
+
+  function handleDeleteTodo(index) {
+    const newTodoList = todos.filter((todo, todoIndex) => {
+      return todoIndex !== index
+    })
+    persistData(newTodoList)
+    setTodos(newTodoList)
+  }
+
+  function handleEditTodo(index) {
+    const valueToBeEdited = todos[index]
+    setTodoValue(valueToBeEdited)
+    handleDeleteTodo(index)
+  }
+
+  function persistData(newList) {
+    localStorage.setItem('todos', JSON.stringify({ todos: newList }))
+  }
 
   return (
-    <ul className='main'>
-      {todos.map((todo, todoIndex) => {
-        return (
-          <TodoCard {...props} key={todoIndex} index={todoIndex}>
-            <p>{todo}</p>
-          </TodoCard>
-        )
-      })}
-    </ul>
+    <>
+      <TodoInput todoValue={todoValue} setTodoValue={setTodoValue} handleAddTodos={handleAddTodos} />
+
+      <ul className='main'>
+        {todos.map((todo, todoIndex) => {
+          return (
+            <TodoCard
+              handleDeleteTodo={handleDeleteTodo}
+              handleEditTodo={handleEditTodo}
+              key={todoIndex}
+              index={todoIndex}
+            >
+              <p>{todo}</p>
+            </TodoCard>
+          )
+        })}
+      </ul>
+    </>
   )
 }
 
